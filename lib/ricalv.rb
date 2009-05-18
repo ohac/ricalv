a79b81a3be03c89e766e4cfa161de9b4a4efb97b
OHASHI Hideya <ohachige@gmail.com>
2009-05-18 20:39:41 +0900

    colored text

--- lib/ricalv.rb
+++ lib/ricalv.rb
@@ -31,6 +31,10 @@ class Date
 end
 end
 
+def escseq(str = '')
+  "\x1b[#{str}m"
+end
+
 WEEKTABLE = { 'SU' => 7, 'MO' => 1, 'TU' => 2,
               'WE' => 3, 'TH' => 4, 'FR' => 5, 'SA' => 6 }
 KEYS = [ 'FREQ', 'BYDAY', 'BYMONTH', 'WKST', 'UNTIL', 'INTERVAL' ]
@@ -69,12 +73,14 @@ calss = icss.map { |fn|
   File.open(fn) { |f| Icalendar.parse(f) }
 }
 
+colors = [ '33', '35', '36', '37' ] * 10 # TODO
 items = calss.map { |cals|
+  color = colors.shift
   cals.map { |cal|
     cal.events.map { |event|
       lines = []
       d = Date.parse(event.dtstart.to_s)
-      lines << { :d => d, :e => event }
+      lines << { :d => d, :e => event, :color => color }
       rrule = event.properties["rrule"]
       if rrule
         rulehash = {}
@@ -120,7 +126,7 @@ items = calss.map { |cals|
               d += 1 while d.cwday != week
               d += (day - 1) * 7
             end
-            lines << { :d => d, :e => event }
+            lines << { :d => d, :e => event, :color => color }
             d = d.next_year
             d = Date.new(d.year, mon, 1) if mon
           end
@@ -131,7 +137,7 @@ items = calss.map { |cals|
   }
 }.flatten
 
-items << {:d => today, :t => '*** TODAY ***'}
+items << {:d => today, :t => '*** TODAY ***', :color => '32' }
 
 items = items.select{|v|
   d = v[:d]
@@ -140,7 +146,18 @@ items = items.select{|v|
 keys = items.map{|v| v[:d]}.uniq
 items = keys.inject({}){|h,v| h[v] = items.select{|x| x[:d] == v}; h}
 items = items.map{|k,v|
-  s = v.map{|x| x[:t] || x[:e].summary}.join(', ')
-  "#{k}(#{WEEKTOSTR[k.wday]}) #{s}"
+  s = v.map{|x|
+    escseq(x[:color]) + (x[:t] || x[:e].summary) + escseq()
+  }.join(', ')
+  w = WEEKTOSTR[k.wday]
+  wc = case k.wday
+  when 0; '31'
+  when 6; '36'
+  end
+  w = escseq(wc) + w + escseq() if wc
+  "#{k}(#{w}) #{s}"
 }
+
+print escseq()
 items.sort.each{|v| puts v}
+print escseq()
